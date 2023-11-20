989c6921a126420aa4a3ee95713b79c348c3a8fa
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-20 15:45:05 +0200

    created MainNav, Navbar components

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,8 @@
-import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
+import Navbar from "@/components/Navbar";
+import prismadb from "@/lib/prismadb";
 
 export default async function DashboardLayout({
   children,
@@ -28,7 +30,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This will be a Navbar</div>
+      <Navbar />
       {children}
     </>
   );
---
+++ components/MainNav.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+const MainNav = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) => {
+  const pathname = usePathname();
+  const params = useParams();
+
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
+
+  return (
+    <nav className={cn(" flex items-center space-x-4 lg:space-x-6", className)}>
+      {routes.map(({ label, href, active }) => (
+        <Link
+          key={href}
+          href={href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            active ? "text-black dark:text-white" : "text-muted-foreground"
+          )}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default MainNav;
