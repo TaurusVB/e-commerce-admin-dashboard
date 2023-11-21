3a190ecba3afd84879452c5ed004bd1cf20f56c3
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-21 15:25:54 +0200

    created prisma billboard model | created imageUpload compoenent | created route for billboard | billboard functionality

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
+import { useParams, useRouter } from "next/navigation";
+
+import axios from "axios";
+
+import { Billboard } from "@prisma/client";
+import { Trash } from "lucide-react";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useOrigin } from "@/hooks/useOrigin";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import AlertModal from "@/components/modals/alertModal";
+import { ApiAlert } from "@/components/ui/apiAlert";
+import ImageUpload from "@/components/ui/imageUpload";
+
+const formSchema = z.object({
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
+});
+
+type BillboardFormValues = z.infer<typeof formSchema>;
+
+interface IBillboardFormProps {
+  initialData: Billboard | null;
+}
+
+const BillboardForm: FC<IBillboardFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+  const origin = useOrigin();
+
+  const description = initialData ? "Edit a billboard" : "Add a new billboard";
+  const title = initialData ? "Edit billboard" : "Create billboard";
+  const toastMessage = initialData
+    ? "Billboard updated."
+    : "Billboard created.";
+  const action = initialData ? "Save changes" : "Create";
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<BillboardFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || { label: "", imageUrl: "" },
+  });
+
+  const onSubmit = async (data: BillboardFormValues) => {
+    setIsLoading(true);
+
+    try {
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/billboards/${params.billboardId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, data);
+      }
+      router.refresh();
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("Something went wrong...");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    setIsLoading(true);
+    setIsOpen(true);
+
+    try {
+      await axios.delete(
+        `/api/${params.storeId}/billboards/${params.billboardId}`
+      );
+      router.refresh();
+      router.push("/");
+      toast.success("Billboard deleted!");
+    } catch (error) {
+      toast.error(
+        "Make sure you removed all categories using billboard first!"
+      );
+    } finally {
+      setIsLoading(false);
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        isLoading={isLoading}
+        onConfirm={onDelete}
+      />
+      <div className=" flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={isLoading}
+            variant="destructive"
+            size="icon"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={isLoading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className=" grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Label</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Billboard label"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" disabled={isLoading} className="ml-auto">
+            {action}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  );
+};
+
+export default BillboardForm;
---
+++ components/ui/imageUpload.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { FC, useEffect, useState } from "react";
+
+import Image from "next/image";
+import { CldUploadWidget } from "next-cloudinary";
+
+import { ImagePlus, Trash } from "lucide-react";
+
+import { Button } from "./button";
+
+interface IImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: FC<IImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className="mb-4 items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+          >
+            <div className=" z-10 absolute top-2 right-2">
+              <Button
+                variant="destructive"
+                size="icon"
+                type="button"
+                onClick={() => onRemove(url)}
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image fill className="object-cover" alt="Image" src={url} />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="r8ms207d">
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+
+          return (
+            <Button
+              type="button"
+              disabled={disabled}
+              variant="secondary"
+              onClick={onClick}
+            >
+              <ImagePlus className="w-4 h-4 mr-2" />
+              Upload an Image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+};
+
+export default ImageUpload;
