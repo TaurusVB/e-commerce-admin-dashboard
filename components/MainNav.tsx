eaa3cce2469580441e451c2bd2408a9571aa601c
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-21 11:39:44 +0200

    added overvie route in MainNav component

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -13,6 +13,11 @@ const MainNav = ({
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
