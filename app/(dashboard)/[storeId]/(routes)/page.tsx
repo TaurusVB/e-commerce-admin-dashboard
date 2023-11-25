e2cff70a52e32e003d3ba7946a6d4de66f1e8a4a
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-25 21:19:48 +0200

    added Card component from shadcn/ui

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,18 +1,22 @@
-import prismadb from "@/lib/prismadb";
 import { FC } from "react";
+
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 
 interface IDashboardPageProps {
   params: { storeId: string };
 }
 
 const DashboardPage: FC<IDashboardPageProps> = async ({ params }) => {
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId,
-    },
-  });
-
-  return <div>Active store: {store?.name}</div>;
+  return (
+    <div className="flex-col">
+      <div className=" flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3"></div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
