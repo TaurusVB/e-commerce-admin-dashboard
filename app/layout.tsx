931e9084ce8716165e85dc497d80a12e88c57d29
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-25 11:12:31 +0200

    fix some date from dashboard API

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "E-commerce ADMIN-DASHBOARD",
+  title: "ADMIN-DASHBOARD",
   description: "E-commerce ADMIN-DASHBOARD",
 };
 
